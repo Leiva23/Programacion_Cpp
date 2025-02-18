b3615fc4e955c874dd846a996c5c76479c53698d
Alex Leiva <alex.leiva@enti.cat>
2025-02-18 21:25:43 +0100

    Add files via upload

---
+++ Exercicis_cpp/Memoria_Dinamica/Exercici_03.cpp
@@ -0,0 +1,11 @@
+///*
+//Crea un programa que pedirá números de 2 en 2 al usuario. Para cada
+//grupo de 2 números el programa almacenará en una estructura distinta los
+//números. Al finalizar debe de mostrar para cada par de números cuál era el
+//mayor de ambos.
+//*/
+//
+//
+//#include <iostream>
+//#include <vector>
+//#i
