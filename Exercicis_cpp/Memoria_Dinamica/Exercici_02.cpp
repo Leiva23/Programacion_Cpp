545b016fd8be0d83ad26bfa1117755cd6a202fdb
Alex Leiva <alex.leiva@enti.cat>
2025-02-18 21:26:30 +0100

    Add files via upload

---
+++ Exercicis_cpp/Memoria_Dinamica/Exercici_02.cpp
@@ -0,0 +1,32 @@
+///*
+//Crea un programa que pida una cifra al usuario. El programa generará
+//aleatoriamente y almacenará tantas cifras como haya indicado el usuario.
+//Tras ello mostrará todas las cifras generadas.
+//*/
+//
+//#include <iostream>
+//#include <vector>
+//#include <time.h>
+//using namespace std;
+//
+//int main() {
+//	srand(time(NULL));
+//	vector<int> numeros;
+//	short numero;
+//
+//	cout << "Introduce un numero para generar una cantidad aleatoria de numeros" << endl;
+//	cin >> numero;
+//
+//	for (int i = 0; i < numero; i++) {
+//		
+//		short random = rand() % numero;
+//		numeros.push_back(random); 
+//	}
+//
+//	cout << "Lista: " << endl;
+//	for (int i = 0; i < numeros.size(); i++) {
+//		cout << numeros[i] << " ";
+//	}
+//
+//	return 0;
+//}
